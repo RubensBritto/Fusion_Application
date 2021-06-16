b17389342142976fe8caa2632ad66dcea53b0118
RubensBritto <jrsb@ic.ufal.br>
2021-06-16 11:11:25 -0300

    Projeto Finalizado

---
+++ core/views.py
@@ -0,0 +1,26 @@
+from django.contrib import messages
+from django.views.generic import FormView
+from .models import Servico, Funcionario
+from .forms import ContatoForms
+from django.urls import reverse_lazy
+
+class IndexView(FormView):
+    template_name = 'index.html'
+    form_class = ContatoForms
+    success_url = reverse_lazy('index')
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_email()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Error ao enviar o E-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+
