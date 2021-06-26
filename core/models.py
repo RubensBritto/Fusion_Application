723342f313102c9673b81026abe0bc38fec328e3
RubensBritto <jrsb@ic.ufal.br>
2021-06-26 15:39:09 -0300

    Traduçao add

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from stdimage.models import StdImageField
 import uuid
+from django.utils.translation import gettext_lazy as _
+
 
 def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
@@ -8,56 +10,56 @@ def get_file_path(_instance, filename):
     return filename
 
 class Base(models.Model):
-    criado = models.DateField('Criação', auto_now_add=True)
-    modificado = models.DateField('Atualização', auto_now=True)
-    ativo = models.BooleanField('Ativo?', default=True)
+    criado = models.DateField(_('Criação'), auto_now_add=True)
+    modificado = models.DateField(_('Atualização'), auto_now=True)
+    ativo = models.BooleanField(_('Ativo?'), default=True)
 
     class Meta:
         abstract = True
 
 class Servico(Base):
     ICONE_CHOICES = (
-        ('lni-cog', 'Engrenagem'),
-        ('lni-status-up', 'Grafico'),
-        ('lni-users', 'Usuario'),
-        ('lni-layers', 'Design'),
-        ('lni-mobile', 'Mobile'),
-        ('lni-rocket', 'Foguete'),
+        ('lni-cog', _('Engrenagem')),
+        ('lni-status-up', _('Grafico')),
+        ('lni-users', _('Usuario')),
+        ('lni-layers', _('Design')),
+        ('lni-mobile', _('Mobile')),
+        ('lni-rocket', _('Foguete')),
     )
 
-    servico = models.CharField('Serviço', max_length=100)
-    descricao = models.TextField('Descrição', max_length=200)
-    icone = models.CharField('Icone', max_length=13, choices=ICONE_CHOICES)
+    servico = models.CharField(_('Serviço'), max_length=100)
+    descricao = models.TextField(_('Descrição'), max_length=200)
+    icone = models.CharField(_('Icone'), max_length=13, choices=ICONE_CHOICES)
 
     class Meta:
-        verbose_name = 'Serviço'
-        verbose_name_plural = 'Serviços'
+        verbose_name = _('Serviço')
+        verbose_name_plural = _('Serviços')
 
     def __str__(self):
         return self.servico
 
 class Cargo(Base):
-    cargo = models.CharField('Cargo', max_length=100)
+    cargo = models.CharField(_('Cargo'), max_length=100)
 
     class Meta:
-        verbose_name = 'Cargo'
-        verbose_name_plural = 'Cargos'
+        verbose_name = _('Cargo')
+        verbose_name_plural = _('Cargos')
 
     def __str__(self):
         return self.cargo
 
 class Funcionario(Base):
-    nome = models.CharField('Nome', max_length=100)
-    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
-    bio = models.TextField('Bio', max_length=200)
-    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    nome = models.CharField(_('Nome'), max_length=100)
+    cargo = models.ForeignKey('core.Cargo', verbose_name=_('Cargo'), on_delete=models.CASCADE)
+    bio = models.TextField(_('Bio'), max_length=200)
+    imagem = StdImageField(_('Imagem'), upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
     facebook = models.CharField('Facebook', max_length=100, default='#')
     twitter = models.CharField('Twitter', max_length=100, default='#')
     instagram = models.CharField('Instagram', max_length=100, default='#')
 
     class Meta:
-        verbose_name = 'Funcionário'
-        verbose_name_plural = 'Funcionários'
+        verbose_name = _('Funcionário')
+        verbose_name_plural = _('Funcionários')
 
     def __str__(self):
         return self.nome
@@ -65,16 +67,16 @@ class Funcionario(Base):
     
 class Feature(Base):
     ICONE_CHOICES_FEATURES = (
-        ('lni-rocket', 'Foguete'),
-        ('lni-laptop-phone', 'Lapto-Phone'),
-        ('lni-cog', 'Engrenagem'),
-        ('lni-leaf', 'Folha'),
-        ('lni-layers', 'Camadas'),
+        ('lni-rocket', _('Foguete')),
+        ('lni-laptop-phone', _('Lapto-Phone')),
+        ('lni-cog', _('Engrenagem')),
+        ('lni-leaf', _('Folha')),
+        ('lni-layers', _('Camadas')),
     )
 
-    nome_feature = models.CharField('Features', max_length=100)
-    descricao_feature = models.TextField('Descrição', max_length=200)
-    icone_feature = models.CharField('Icone', max_length=20, choices=ICONE_CHOICES_FEATURES)
+    nome_feature = models.CharField(_('Features'), max_length=100)
+    descricao_feature = models.TextField(_('Descrição'), max_length=200)
+    icone_feature = models.CharField(_('Icone'), max_length=20, choices=ICONE_CHOICES_FEATURES)
 
     def __str__(self):
         return self.nome_feature
